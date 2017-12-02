6018eeb8d50940e138445169d82b9c18ff45d783
Carl Ruiz <carl.45.cr@gmail.com>
2017-12-02 13:09:09 +0100

    clear service gnome

--- src/app/home/services/gnome.service.ts
+++ src/app/home/services/gnome.service.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-
 import { api } from './../../constants/api.constants';
 
 @Injectable()
